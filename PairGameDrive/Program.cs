6d026146fe0fe6addaf5d22dc9b61a5dd715b240
GavinAnkney <gavinmartinankney@gmail.com>
2021-12-07 20:05:43 -0500

    Second Commit

--- PairGameDrive/Program.cs
+++ PairGameDrive/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PairGameDrive
 {
@@ -68,5 +65,101 @@ namespace PairGameDrive
                 Console.CursorVisible = true;
             }
         }
+        static void Initialize()
+        {
+            windowWidth = Console.WindowWidth;
+            windowHeight = Console.WindowHeight;
+            if (OperatingSystem.IsWindows())
+            {
+                if (windowWidth < width && OperatingSystem.IsWindows())
+                {
+                    windowWidth = Console.WindowWidth = width + 1;
+                }
+                if (windowHeight < height && OperatingSystem.IsWindows())
+                {
+                    windowHeight = Console.WindowHeight = height + 1;
+                }
+                Console.BufferWidth = windowWidth;
+                Console.BufferWidth = windowHeight;
+            }
+        }
+        static void InitializeScene()
+        {
+            const int roadWidth = 10;
+            gameRunning = true;
+            carPosition = width / 2;
+            carVelocity = 0;
+            int leftEdge = (width - roadWidth) / 2;
+            int rightEdge = leftEdge + roadWidth + 1;
+            scene = new char[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (j < leftEdge || j > rightEdge)
+                    {
+                        scene[i, j] = '.';
+                    }
+                    else
+                    {
+                        scene[i, j] = ' ';
+                    }
+                }
+            }
+        }
+        static void Render()
+        {
+            StringBuilder stringBuilder = new(width * height);
+            for (int i = height - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 1 && j == carPosition)
+                    {
+                        stringBuilder.Append(!gameRunning ? 'X' :
+                            carVelocity < 0 ? '<' :
+                            carVelocity > 0 ? '>' :
+                            '^');
+                    }
+                    else
+                    {
+                        stringBuilder.Append(scene[i, j]);
+                    }
+                }
+                if (i > 0)
+                {
+                    stringBuilder.AppendLine();
+                }
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.Write(stringBuilder);
+        }
+        static void HandleInput()
+        {
+            while (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.A or ConsoleKey.LeftArrow:
+                        carVelocity = -1;
+                        break;
+                    case ConsoleKey.D or ConsoleKey.RightArrow:
+                        carVelocity = +1;
+                        break;
+                    case ConsoleKey.W or ConsoleKey.UpArrow or ConsoleKey.S or ConsoleKey.DownArrow:
+                        carVelocity = 0;
+                        break;
+                    case ConsoleKey.Escape:
+                        gameRunning = false;
+                        keepPlaying = false;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+
     }
 }
